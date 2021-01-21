eb6beb5bc608ff27516e5ba22a858afcb6c63373
Roman Kulichenko <core.autocrlf>
2021-01-21 22:05:54 +0200

    Refactored validation in admin section

--- shop/backend/shop/admin.py
+++ shop/backend/shop/admin.py
@@ -1,18 +1,16 @@
+from django.forms import ModelChoiceField
 from django import forms
 from django.contrib import admin
 
 from .models import *
 
 
-class BuckwheatCategoryChoiceField(forms.ModelChoiceField):
-    pass
-
 # Убираем ненужные категории при добавлении определённых видов товаров
 class BuckwheatProductAdmin(admin.ModelAdmin):
     
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "category":
-            return BuckwheatCategoryChoiceField(Category.objects.filter(slug="buckwheat"))
+            return ModelChoiceField(Category.objects.filter(slug="buckwheat"))
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 
